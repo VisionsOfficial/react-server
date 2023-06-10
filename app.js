89926e525edd64ccbdce6a2145fe5f437663bbf3
Felix Bole <felix.bole@yahoo.fr>
2023-06-10 13:19:07 +0200

    fixes

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+
+app.enable("trust proxy");
+app.use((req, res, next) => {
+	req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
-	res.sendFile(__dirname, "public", "index.html");
+	res.sendFile(__dirname, "public/index.html");
 });
 
 app.listen(process.env.PORT || 3000, () => {
