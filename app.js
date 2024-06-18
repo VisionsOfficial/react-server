527011c4912f2218451d1614e55b1ea65b915e8e
Felix Bole <felix.bole@yahoo.fr>
2024-06-18 12:08:43 +0200

    initial commit for github

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
 
 const app = express();
-app.use(cors());
 
-app.enable("trust proxy");
-app.use((req, res, next) => {
-	req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
-});
+// app.enable("trust proxy");
+// app.use((req, res, next) => {
+// 	req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+// });
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
-	res.sendFile(__dirname, "public/index.html");
+	res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => {
